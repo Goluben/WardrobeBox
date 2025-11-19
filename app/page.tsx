123776f82ece64bf30c7bb14e915837061ef4ff7
alex <alex.mart2002@gmail.com>
2025-11-19 18:00:23 +0300

    added card

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,8 @@
 import { useCallback } from "react";
 
 import Button from "../src/components/my-button";
-import { Color } from "../src/types";
+import Card from "../src/components/card";
+import { Color, Product } from "../src/types";
 
 export default function App() {
   const handleClick = useCallback(
@@ -11,11 +12,17 @@ export default function App() {
     []
   );
 
+  const item: Product = {
+    id: "123",
+    brand: "NewBalance",
+    model: "2002 NR",
+    price: "135",
+    imgSrc: "https://a.lmcdn.ru/product/R/T/RTLADV243701_24945579_3_v1.jpg",
+  };
+
   return (
     <div>
-      <Button color={Color.Aqua} size="m" onClick={handleClick}>
-        Кнопочка
-      </Button>
+      <Card item={item} onClick={handleClick}></Card>
     </div>
   );
 }
